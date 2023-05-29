760711734aa5c6542bc757c0b66e996dcca55353
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-29 21:43:22 +0530

    commited

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -9,19 +9,17 @@ public class Email {
     private boolean isDigit;
     private boolean isSpecialChar;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
-    public Email(String emailId,String password)
-    {
-        this.emailId=emailId;
-        if(isValidPass(password))
-        {
-            this.password=password;
-        }
-        else {
-            this.password="Accio@123";
+
+    public Email(String emailId, String password) {
+        this.emailId = emailId;
+        if (this.isValidPass(password)) {
+            this.password = password;
+        } else {
+            this.password = "Accio@123";
         }
     }
 
@@ -33,53 +31,45 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(oldPassword.equals(this.password)){
-            if(isValidPass(newPassword))
-            {
+        if (oldPassword.equals(this.password)) {
+            if (this.isValidPass(newPassword)) {
                 System.out.println("Password changed successfully!");
-                this.password=newPassword;
-            }
-            else{
+                this.password = newPassword;
+            } else {
                 System.out.println("The new password is not valid!");
+
             }
         }
 //        else{
 //            System.out.println("The given password does not match current password!");
 //        }
     }
-    public boolean isValidPass(String pass)
-    {
-        int n = pass.length();
-        if(n<8)
-            return false;
-        //check upperCase
-        for(int i=0;i<n;i++)
-        {
-            if(pass.charAt(i)>='A' && pass.charAt(i)<='Z')
-            {
-                isUpperCase=true;
-            }
-            else if(pass.charAt(i)>='a' && pass.charAt(i)<='z')
-            {
-                isLowerCase=true;
-            }
-            else if(pass.charAt(i)>='0' && pass.charAt(i)<='9')
-            {
-                isDigit=true;
-            }
-            else
-            {
-                isSpecialChar=true;
-            }
-        }
 
-        return (isSpecialChar && isDigit && isUpperCase && isLowerCase);
+    public boolean isValidPass(String pass) {
+        int n = pass.length();
+        if (n < 8) {
+            return false;
+        } else {
+            for (int i = 0; i < n; ++i) {
+                if (pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z') {
+                    this.isUpperCase = true;
+                } else if (pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z') {
+                    this.isLowerCase = true;
+                } else if (pass.charAt(i) >= '0' && pass.charAt(i) <= '9') {
+                    this.isDigit = true;
+                } else {
+                    this.isSpecialChar = true;
+                }
+            }
+
+            return this.isSpecialChar && this.isDigit && this.isUpperCase && this.isLowerCase;
+        }
     }
 }
